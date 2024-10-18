502d3337ef9da1d076e9c21747dc8c6a4fdab657
unknown <vijayguhan10@gmail.com>
2024-10-18 21:13:08 +0530

    updated the jwt token

--- frontend/src/Router/InitialRouter.js
+++ frontend/src/Router/InitialRouter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import Dashboard from "../components/Dashboard";
@@ -25,21 +25,56 @@ const InitialRouter = () => {
       return false;
     }
   };
-
   const isAuthenticated = isTokenValid(token);
 
   return (
     <Routes>
-      <Route path="/" element={isAuthenticated ? <Navigate to="/Dashboard" /> : <Login setToken={setToken} />} />
-      <Route path="/Dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} />
-      <Route path="/Placement" element={isAuthenticated ? <Placement /> : <Navigate to="/" />} />
-      <Route path="/Technical" element={isAuthenticated ? <Technical /> : <Navigate to="/" />} />
-      <Route path="/NonTechnical" element={isAuthenticated ? <Nontechnical /> : <Navigate to="/" />} />
-      <Route path="/History" element={isAuthenticated ? <History /> : <Navigate to="/" />} />
-      <Route path="/Form" element={isAuthenticated ? <Forms /> : <Navigate to="/" />} />
-      <Route path="/Editdata" element={isAuthenticated ? <CRUD /> : <Navigate to="/" />} />
-      <Route path="/Departments" element={isAuthenticated ? <Departments /> : <Navigate to="/" />} />
-      <Route path="/CanceledEvents" element={isAuthenticated ? <Canceled /> : <Navigate to="/" />} />
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/Dashboard" />
+          ) : (
+            <Login setToken={setToken} />
+          )
+        }
+      />
+      <Route
+        path="/Dashboard"
+        element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/Placement"
+        element={isAuthenticated ? <Placement /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/Technical"
+        element={isAuthenticated ? <Technical /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/NonTechnical"
+        element={isAuthenticated ? <Nontechnical /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/History"
+        element={isAuthenticated ? <History /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/Form"
+        element={isAuthenticated ? <Forms /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/Editdata"
+        element={isAuthenticated ? <CRUD /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/Departments"
+        element={isAuthenticated ? <Departments /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/CanceledEvents"
+        element={isAuthenticated ? <Canceled /> : <Navigate to="/" />}
+      />
     </Routes>
   );
 };
