d64404a11c0cef50b2bcac940e9768ca43a605fc
unknown <vijayguhan10@gmail.com>
2024-10-19 11:39:37 +0530

    updating consistelty twilio

--- Backend/other/Whatsapp.js
+++ Backend/other/Whatsapp.js
@@ -6,6 +6,25 @@ const client = new twilio(
   process.env.TWILIO_AUTH_TOKEN
 );
 
+const departmentOptions = [
+  { fullName: "Computer and Communication Engineering", shortName: "CCE" },
+  { fullName: "Computer Science Engineering", shortName: "CSE" },
+  {
+    fullName: "Artificial Intelligence and Data Science",
+    shortName: "AI & DS",
+  },
+  { fullName: "Electronics and Communication Engineering", shortName: "ECE" },
+  { fullName: "Information Technology", shortName: "IT" },
+  { fullName: "Mechanical Engineering", shortName: "MECH" },
+  {
+    fullName: "Artificial Intelligence and Machine Learning",
+    shortName: "AI & ML",
+  },
+  { fullName: "Computer Science and Business Systems", shortName: "CSBS" },
+  { fullName: "Electrical and Electronics Engineering", shortName: "EEE" },
+  { fullName: "Cybersecurity", shortName: "Cyber" },
+  { fullName: "All", shortName: "All" },
+];
 const getMessage = async (req, res) => {
   try {
     const message = req.body.Body.trim();
@@ -78,6 +97,51 @@ const getMessage = async (req, res) => {
 
       return res.status(200).send("Message sent.");
     }
+     
+
+    if (message.toLowerCase() === "cse") {
+      const department = eventsToday.filter(
+        (elem) => elem.departments[0] === "Computer Science Engineering"
+      );
+
+      if (department.length === 0) {
+        const noEventsMessage = "No events scheduled for today. for cse department";
+        await client.messages.create({
+          body: noEventsMessage,
+          from: "whatsapp:+14155238886",
+          to: `whatsapp:${num}`,
+        });
+        return res.status(200).send("No events found.");
+      }
+
+      let responseMessage = `Events scheduled for today (${formattedToday}): for cse department\n\n`;
+      department.forEach((event) => {
+        responseMessage += `*Event Name*: ${event.eventname}\n`;
+        responseMessage += `*Type of Event*: ${event.typeofevent}\n`;
+        responseMessage += `*Resource Person*: ${event.resourceperson}\n`;
+        responseMessage += `*Organizer*: ${event.organizer}\n`;
+        responseMessage += `*Venue*: ${event.venue}\n`;
+        responseMessage += `*Event Start*: ${event.eventstartdate} at ${event.eventstarttime}\n`;
+        responseMessage += `*Event End*: ${event.eventenddate} at ${event.eventendtime}\n`;
+        responseMessage += `*Status*: ${event.status}\n\n`;
+      });
+
+      const response = await client.messages.create({
+        body: responseMessage,
+        from: "whatsapp:+14155238886",
+        to: `whatsapp:${num}`,
+      });
+
+      return res.status(200).send("Message sent.");
+    }
+
+
+
+
+
+
+
+
   } catch (err) {
     console.log(err);
     res.status(500).send("Error occurred while sending the message.");
