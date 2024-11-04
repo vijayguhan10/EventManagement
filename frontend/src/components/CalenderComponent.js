a823a6a145109646b621f10fabee5c97d03f0dec
sabarim6369 <sabarim6369@gmail.com>
2024-11-04 09:42:15 +0530

    moved pdf generator to calender component

--- frontend/src/components/CalenderComponent.js
+++ frontend/src/components/CalenderComponent.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "../Calender.css";
 import forwardarrow from "../assets/Forward Arrow.png";
+
+import { FaFilePdf, FaSearch, FaFileExcel } from "react-icons/fa";
 import prevarrow from "../assets/Forward Arrow (1).png";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -15,6 +17,113 @@ const CalendarComponent = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [isFullYear, setIsFullYear] = useState(false);
+  const [selectedYears, setSelectedYears] = useState([]);
+  const [showIcons, setShowIcons] = useState(false);
+  const [departments, setDepartments] = useState([]);
+  const handleDepartmentChange = (event) => {
+    const selectedDeptShortName = event.target.value;
+    if (selectedDeptShortName === "All") {
+      if (event.target.checked) {
+        setDepartments(["All"]);
+      } else {
+        setDepartments([]);
+      }
+    } else {
+      const selectedDeptFullName = departmentOptions.find(
+        (dept) => dept.shortName === selectedDeptShortName
+      ).fullName;
+
+      setDepartments((prevDepartments) => {
+        if (prevDepartments.includes("All")) {
+          return [selectedDeptFullName];
+        }
+
+        return prevDepartments.includes(selectedDeptFullName)
+          ? prevDepartments.filter((dept) => dept !== selectedDeptFullName)
+          : [...prevDepartments, selectedDeptFullName];
+      });
+    }
+  };
+  
+  const departmentOptions = [
+    { fullName: "Computer and Communication Engineering", shortName: "CCE" },
+    { fullName: "Computer Science Engineering", shortName: "CSE" },
+    {
+      fullName: "Artificial Intelligence and Data Science",
+      shortName: "AI & DS",
+    },
+    { fullName: "Electronics and Communication Engineering", shortName: "ECE" },
+    { fullName: "Information Technology", shortName: "IT" },
+    { fullName: "Mechanical Engineering", shortName: "MECH" },
+    {
+      fullName: "Artificial Intelligence and Machine Learning",
+      shortName: "AI & ML",
+    },
+    { fullName: "Computer Science and Business Systems", shortName: "CSBS" },
+    { fullName: "Electrical and Electronics Engineering", shortName: "EEE" },
+    { fullName: "Cybersecurity", shortName: "Cyber" },
+    { fullName: "All", shortName: "All" },
+  ];
+  const handleFullYearChange = () => {
+    setIsFullYear(!isFullYear);
+    if (!isFullYear) {
+      setFromDate("");
+      setToDate("");
+    }
+  };
+  const handleGeneratePDF = async () => {
+    if (departments.length === 0 || selectedYears.length === 0) {
+      setErrorMessage(
+        "Please select at least one department and one year to generate the PDF."
+      );
+      return;
+    }
+
+    if (!isFullYear && (!fromDate || !toDate)) {
+      setErrorMessage("Please select a valid date range to generate the PDF.");
+      return;
+    }
+
+    setErrorMessage("");
+
+    console.log("Selected year for PDF generation:", selectedYears);
+
+    const selectedData = {
+      departments: departments,
+      ...(isFullYear ? { fullYear: true } : { fromDate, toDate }),
+      year: selectedYears.includes("All") ? "All" : selectedYears,
+    };
+
+    console.log("Selected data for PDF generation:", selectedData);
+
+    try {
+      const response = await axios({
+        url: `${process.env.REACT_APP_BASE_URL}/event/generatedpdf-doc`,
+        method: "GET",
+        params: selectedData,
+        responseType: "blob",
+      });
+
+      console.log("PDF generation response:", response);
+
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const link = document.createElement("a");
+
+      link.href = window.URL.createObjectURL(blob);
+      link.download = "events-report.pdf";
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(link.href);
+    } catch (error) {
+      console.error("Error fetching PDF:", error);
+    }
+  };
 
   const onClickDay = (value) => {
     setSelectedDate(value);
@@ -73,7 +182,29 @@ const CalendarComponent = () => {
       eventStartDate.toLocaleDateString() === selectedDate.toLocaleDateString()
     );
   });
-
+  const handleDownloadClick = () => {
+    setShowIcons(!showIcons);
+  }
+  const handleYearChange = (event, year) => {
+    if (year === "All") {
+      if (event.target.checked) {
+        setSelectedYears([1, 2, 3, 4]);
+      } else {
+        // When "All" is deselected, clear the state
+        setSelectedYears([]);
+      }
+    } else {
+      setSelectedYears((prevSelected) => {
+        if (prevSelected.includes(year)) {
+          // If a specific year is deselected
+          return prevSelected.filter((y) => y !== year);
+        } else {
+          // If a specific year is selected
+          return [...prevSelected, year].filter((y) => y !== "All"); // Remove "All" if selecting individual years
+        }
+      });
+    }
+  };
   const monthYearString = currentDate.toLocaleString("default", {
     month: "long",
     year: "numeric",
@@ -122,6 +253,7 @@ const CalendarComponent = () => {
             <img src={forwardarrow} alt="next month" />
           </button>
         </div>
+        
         <Calendar
           onChange={onChange}
           value={selectedDate}
@@ -134,7 +266,7 @@ const CalendarComponent = () => {
           activeStartDate={currentDate}
         />
       </div>
-
+   
       <div className="flex flex-row items-center justify-between w-[90%] xl:h-16 xl:w-[94%] bg-white border-l-8 border-l-[#7848F4] rounded-md shadow-lg shadow-[#00000029] mt-2 transition-transform transform hover:scale-105">
         <div className="ml-3 xl:ml-5 py-5">
           <p className="text-2xl font-bold text-gray-800 font-Afacad">
@@ -305,6 +437,125 @@ const CalendarComponent = () => {
           </div>
         </div>
       )}
+      
+      <div className="container absolute bottom-[-80%] left-[55%] w-[43%] mx-auto p-4 border-black rounded-xl shadow-lg z-50">
+      <h1 className="text-xl font-bold text-center text-black ">
+    Department Report Generator
+  </h1>
+  {errorMessage && (
+    <p className="text-red-600 text-center mt-1">{errorMessage}</p>
+  )}
+  
+  {/* Date Range Selection and Full Year Option */}
+  <div className="flex justify-between items-center space-x-4 mb-2">
+    {/* From Date */}
+    <div>
+      <h2 className="text-xl font-semibold text-gray-700">From Date</h2>
+      <input
+        type="date"
+        value={fromDate}
+        onChange={(e) => setFromDate(e.target.value)}
+        className="p-2 border rounded-lg focus:outline-none w-48 text-xl h-10 focus:ring-2 focus:ring-green-400"
+        disabled={isFullYear}
+      />
+    </div>
+
+    {/* To Date */}
+    <div>
+      <h2 className="text-xl font-semibold text-gray-700">To Date</h2>
+      <input
+        type="date"
+        value={toDate}
+        onChange={(e) => setToDate(e.target.value)}
+        className="p-2 border rounded-lg focus:outline-none w-48 text-xl h-10 focus:ring-2 focus:ring-green-400"
+        disabled={isFullYear}
+      />
+    </div>
+
+    {/* Full Year Option */}
+    <div className="flex items-center space-x-2">
+      <input
+        type="checkbox"
+        checked={isFullYear}
+        onChange={handleFullYearChange}
+        className="form-checkbox h-4 w-4 text-green-600"
+      />
+      <label className="text-gray-700 text-xl font-semibold">Full Year</label>
+    </div>
+  </div>
+
+  {/* Department Selection */}
+  <div className="mb-2">
+    <h2 className="text-lg font-semibold text-gray-700 mb-2">Departments</h2>
+    <div className="flex flex-wrap gap-4">
+      {departmentOptions.map((department) => (
+        <div key={department.shortName} className="flex items-center font-bold space-x-2">
+          <input
+            type="checkbox"
+            value={department.shortName}
+            checked={departments.includes(department.fullName)}
+            onChange={handleDepartmentChange}
+            className="form-checkbox font-bold h-4 w-4 text-green-600"
+            disabled={departments.includes("All") && department.shortName !== "All"}
+          />
+          <span className="text-gray-700 font-bold text-lg">
+            {department.shortName}
+          </span>
+        </div>
+      ))}
+    </div>
+  </div>
+
+  {/* Year Selection */}
+ {/* Year Selection */}
+<div className="mb-4 flex">
+  <h2 className="text-xl font-semibold text-gray-700 mb-2 pr-4">Year</h2>
+  <div className="flex gap-2">
+    {[1, 2, 3, 4, "All"].map((year) => (
+      <div key={year} className="flex items-center space-x-3"> {/* Adjusted space-x */}
+        <input
+          type="checkbox"
+          value={year}
+          checked={year === "All" ? selectedYears.length === 4 : selectedYears.includes(year)}
+          onChange={(e) => handleYearChange(e, year)}
+          className="form-checkbox h-4 w-4 text-green-600"
+          disabled={selectedYears.includes("All") && year !== "All"}
+        />
+        <label className="text-gray-700 text-lg">{year}</label>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+  {/* Download Section */}
+  <div className="text-center mb-4 flex items-center space-x-4">
+    {showIcons && (
+      <>
+        <FaFilePdf
+          size={34}
+          color="#7312f1d3"
+          onClick={handleGeneratePDF}
+          className="cursor-pointer hover:scale-105 transition-transform duration-300"
+        />
+        <FaFileExcel
+          size={34}
+          color="#7312f1d3"
+          onClick={handleGeneratePDF}
+          className="cursor-pointer hover:scale-105 transition-transform duration-300"
+        />
+      </>
+    )}
+    <button
+      type="button"
+      onClick={handleDownloadClick}
+      className="focus:outline-none text-white bg-[#7312f1d3] hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-3 py-1.5 mb-2 transition-all duration-300"
+    >
+      {showIcons ? 'Hide' : 'Download'}
+    </button>
+  </div>
+</div>
+
     </div>
   );
 };
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -293,7 +293,7 @@ const Dashboard = () => {
             </h1>
             <h1 className="text-xl font-Afacad mt-3 font-bold">Todays Data</h1>
           </div>
-          {/* <div className="relative ml-[58%] mb-32 pl-">
+          <div className="relative ml-[100%] mb-32 pl-">
             <input
               type="text"
               placeholder="Search events..."
@@ -307,7 +307,7 @@ const Dashboard = () => {
             <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-purple-600 hover:bg-purple-700 text-white px-4 py-1 rounded-md">
               Search
             </button>
-          </div> */}
+          </div>
         </div>
         <div className="xl:ml-72 h-80 mt-5 xl:w-[80%] w-full bg-white">
           <div className="mx-auto p-0">
@@ -426,123 +426,6 @@ const Dashboard = () => {
   </div>
 )}
 
-<div className="container absolute bottom-[-1%] left-[55%] w-[43%] mx-auto p-4 border-black rounded-xl shadow-lg">
-  <h1 className="text-xl font-bold text-center text-black ">
-    Department Report Generator
-  </h1>
-  {errorMessage && (
-    <p className="text-red-600 text-center mt-1">{errorMessage}</p>
-  )}
-  
-  {/* Date Range Selection and Full Year Option */}
-  <div className="flex justify-between items-center space-x-4 mb-2">
-    {/* From Date */}
-    <div>
-      <h2 className="text-xl font-semibold text-gray-700">From Date</h2>
-      <input
-        type="date"
-        value={fromDate}
-        onChange={(e) => setFromDate(e.target.value)}
-        className="p-2 border rounded-lg focus:outline-none w-48 text-xl h-10 focus:ring-2 focus:ring-green-400"
-        disabled={isFullYear}
-      />
-    </div>
-
-    {/* To Date */}
-    <div>
-      <h2 className="text-xl font-semibold text-gray-700">To Date</h2>
-      <input
-        type="date"
-        value={toDate}
-        onChange={(e) => setToDate(e.target.value)}
-        className="p-2 border rounded-lg focus:outline-none w-48 text-xl h-10 focus:ring-2 focus:ring-green-400"
-        disabled={isFullYear}
-      />
-    </div>
-
-    {/* Full Year Option */}
-    <div className="flex items-center space-x-2">
-      <input
-        type="checkbox"
-        checked={isFullYear}
-        onChange={handleFullYearChange}
-        className="form-checkbox h-4 w-4 text-green-600"
-      />
-      <label className="text-gray-700 text-xl font-semibold">Full Year</label>
-    </div>
-  </div>
-
-  {/* Department Selection */}
-  <div className="mb-2">
-    <h2 className="text-lg font-semibold text-gray-700 mb-2">Departments</h2>
-    <div className="flex flex-wrap gap-4">
-      {departmentOptions.map((department) => (
-        <div key={department.shortName} className="flex items-center font-bold space-x-2">
-          <input
-            type="checkbox"
-            value={department.shortName}
-            checked={departments.includes(department.fullName)}
-            onChange={handleDepartmentChange}
-            className="form-checkbox font-bold h-4 w-4 text-green-600"
-            disabled={departments.includes("All") && department.shortName !== "All"}
-          />
-          <span className="text-gray-700 font-bold text-lg">
-            {department.shortName}
-          </span>
-        </div>
-      ))}
-    </div>
-  </div>
-
-  {/* Year Selection */}
- {/* Year Selection */}
-<div className="mb-4 flex">
-  <h2 className="text-xl font-semibold text-gray-700 mb-2 pr-4">Year</h2>
-  <div className="flex gap-2">
-    {[1, 2, 3, 4, "All"].map((year) => (
-      <div key={year} className="flex items-center space-x-3"> {/* Adjusted space-x */}
-        <input
-          type="checkbox"
-          value={year}
-          checked={year === "All" ? selectedYears.length === 4 : selectedYears.includes(year)}
-          onChange={(e) => handleYearChange(e, year)}
-          className="form-checkbox h-4 w-4 text-green-600"
-          disabled={selectedYears.includes("All") && year !== "All"}
-        />
-        <label className="text-gray-700 text-lg">{year}</label>
-      </div>
-    ))}
-  </div>
-</div>
-
-
-  {/* Download Section */}
-  <div className="text-center mb-4 flex items-center space-x-4">
-    {showIcons && (
-      <>
-        <FaFilePdf
-          size={34}
-          color="#7312f1d3"
-          onClick={handleGeneratePDF}
-          className="cursor-pointer hover:scale-105 transition-transform duration-300"
-        />
-        <FaFileExcel
-          size={34}
-          color="#7312f1d3"
-          onClick={handleGeneratePDF}
-          className="cursor-pointer hover:scale-105 transition-transform duration-300"
-        />
-      </>
-    )}
-    <button
-      type="button"
-      onClick={handleDownloadClick}
-      className="focus:outline-none text-white bg-[#7312f1d3] hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-sm px-3 py-1.5 mb-2 transition-all duration-300"
-    >
-      {showIcons ? 'Hide' : 'Download'}
-    </button>
-  </div>
-</div>
 
     </div>
   );
