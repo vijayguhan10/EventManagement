531edfc6b896d511b6e785dc095f807a37fbcd7e
sabarim6369 <sabarim6369@gmail.com>
2024-11-15 00:01:09 +0530

    one minute

--- Backend/other/Whatsapp.js
+++ Backend/other/Whatsapp.js
@@ -167,7 +167,7 @@ const SendAutoScheduling = async () => {
   }
 };
 // Schedule the job to run at 11:37 PM every day
-cron.schedule("55 23 * * *", () => {
+cron.schedule("* * * * *", () => {
   console.log("Scheduled job running at 11:37 PM...");
   SendAutoScheduling();
 });
