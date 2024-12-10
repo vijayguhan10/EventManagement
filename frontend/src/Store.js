a08d5d0adf169b2e9af366d57543d228a24b9fe8
unknown <vijayguhan10@gmail.com>
2024-12-10 12:56:29 +0530

    updated the form values

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,6 +1,5 @@
 import eventReducer from "./redux/reducers/eventReducers";
 import { configureStore } from "@reduxjs/toolkit";
-//adeded the changes
 const store = configureStore({
   reducer: {
     events: eventReducer,
