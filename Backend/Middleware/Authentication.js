ff0aee6784600fcd11ab78f0247778d6cbfb8b0d
vijaygugan10 <vijayguhan10@gmail.com>
2025-01-22 17:57:59 +0530

    checking with computer

--- Backend/Middleware/Authentication.js
+++ Backend/Middleware/Authentication.js
@@ -6,7 +6,7 @@ const jwtMiddleware = async (req, res, next) => {
   if (!token) {
     return res.status(401).json({ message: "No token provided." });
   }
-
+console.log("ggkg")
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
     req.userId = decoded.userId;
